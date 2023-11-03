f2cdc9530661e9d484a5dd5359f227d9b387bb96
XinpengW <william.xp.wei@gatech.edu>
2023-11-03 06:44:55 -0600

    update swarm setup script

--- setup_docker_swarm.py
+++ setup_docker_swarm.py
@@ -7,13 +7,13 @@ import re
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--number', type=int, required=True, help='Docker Swarm Cluter Size')
-    parser.add_argument('-ip', '--ip', type=str, required=True, help='Mgr IP')
+    parser.add_argument('-a', '--ip', type=str, required=True, help='Mgr IP')
     return parser.parse_args()
 
 install_docker = '''sudo apt-get update && \
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install \
 ca-certificates curl gnupg lsb-release && \
-sudo mkdir -p /etc/apt/keyrings && \
+sudo install -m 0755 -d /etc/apt/keyrings && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
 echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
@@ -21,20 +21,20 @@ sudo apt-get update && \
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
 docker-ce docker-ce-cli containerd.io'''
 args = parse_args()
-# proc = subprocess.Popen(shlex.split(install_docker))
 
-with ThreadingGroup(*[f'node-{idx}' for idx in range(1, args.number)]) as grp:
-    # res = grp.run(install_docker)
-    # while True:
-    #     try:
-    #         proc.wait(1)
-    #     except subprocess.TimeoutExpired as timeout:
-    #         continue
-    #     break
+with ThreadingGroup(*[f'node-{idx}' for idx in range(0, args.number)]) as grp:
+    res = grp.run(install_docker)
     def stop_swarm_cluster():
         grp.run('sudo docker swarm leave')
         subprocess.run(shlex.split('sudo docker swarm leave -f'))
     # stop_swarm_cluster()
+    def clear_env():
+        grp.run('sudo apt-get -y purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras')
+        grp.run('for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done')
+        grp.run('sudo rm -rf /var/lib/containerd')
+        grp.run('sudo rm -rf /var/lib/docker')
+        grp.run('sudo rm /etc/apt/keyrings/docker.gpg')
+    # uninstall_docker()
     ret = subprocess.run(['sudo', 'docker', 'swarm', 'init', '--advertise-addr', args.ip], capture_output=True)
     swarm_join_cmd_ptn = r'(docker swarm join --token .*:\d+)'
     swarm_join_cmd = re.search(swarm_join_cmd_ptn, ret.stdout.decode('utf-8'))
@@ -44,4 +44,5 @@ with ThreadingGroup(*[f'node-{idx}' for idx in range(1, args.number)]) as grp:
         exit(0)
     else:
         swarm_join_cmd = swarm_join_cmd.group()
-    grp.run('sudo ' + swarm_join_cmd)
+    with ThreadingGroup.from_connections(grp[1:]) as grp_worker:
+        grp_worker.run('sudo ' + swarm_join_cmd)
